61f56f4f281ba5e11d4bdfbb7af12db618684b29
PauloSeb <paul.sebastien974@gmail.com>
2014-02-20 11:25:18 +0100

    variables

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-//Global var
-var lampeHalogeneTimeout = 3000;
-var devices = new Array();
+//Global vars
+var uPnPdevices = new Array();
+var sensor = new Array();
+
 //REST
 var express = require('express');
 var app = express();
 app.listen(5000);
 console.log("Server listening on port 5000");
-//UPNP
+
+//UPnP
 var UpnpControlPoint = require("../lib/upnp-controlpoint").UpnpControlPoint;
 var cp = new UpnpControlPoint();
 cp.search();
 cp.on("device", function(device){
-	devices[device.deviceType] = device;
+	uPnPdevices[device.deviceType] = device;
 });
